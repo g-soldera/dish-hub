3b558ab83c9697e43b86f55b28c3b50c91d3b241
Gustavo Soldera <gustavosevere@gmail.com>
2023-06-17 00:15:38 -0300

    feat: add header

---
+++ src/components/header.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import styled from "styled-components"
+import { SearchInput } from "./searchInput"
+
+interface HeaderProps {}
+
+const TagHeader = styled.header`
+  background-color: var(--secondary-bg);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1.2rem 10rem;
+  width: 100%;
+
+  svg {
+    cursor: pointer;
+    width: 1.7rem;
+    stroke: var(--primary-text);
+  }
+
+  svg:hover {
+    stroke: var(--main-color);
+  }
+`
+
+const Logo = styled.h1`
+  width: 30%;
+  display: flex;
+  align-items: center;
+  font-size: 1.75rem;
+  font-weight: 700;
+
+  span {
+    margin-left: 0.3rem;
+    color: var(--secondary-bg);
+    background-color: var(--main-color);
+    padding: 0.1rem 0.35rem;
+    border-radius: var(--border-radius);
+  }
+`
+
+export function Header(props: HeaderProps) {
+  return (
+    <>
+      <TagHeader>
+        <Logo>
+          <a href="/">
+            Dish<span>Hub</span>
+          </a>
+        </Logo>
+        <SearchInput placeholder="Search" />
+      </TagHeader>
+    </>
+  )
+}
---
+++ src/components/searchInput.tsx
@@ -0,0 +1,63 @@
+import styled from "styled-components"
+import { SearchIcon } from "./searchIcon"
+import { FavoritesIcon } from "./favsIcon"
+import { InputHTMLAttributes } from "react"
+
+const SearchArea = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+`
+
+const SearchBar = styled.div`
+  padding: 0.6rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: var(--primary-bg);
+  border-radius: var(--border-radius);
+  border: none;
+  width: 22vw;
+
+  svg {
+    cursor: pointer;
+    height: 1.6rem;
+    width: 1.2rem;
+    fill: var(--primary-text);
+  }
+`
+
+const Search = styled.input`
+  font-family: inherit;
+  font-weight: 400;
+  line-height: 1.25rem;
+
+  background-color: transparent;
+  outline: none;
+  border: none;
+  width: 85%;
+
+  &:hover {
+    outline: none;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`
+
+interface SearchInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function SearchInput(props: SearchInputProps) {
+  return (
+    <>
+      <SearchArea>
+        <SearchBar>
+          <Search {...props} />
+          <SearchIcon />
+        </SearchBar>
+        <FavoritesIcon />
+      </SearchArea>
+    </>
+  )
+}
